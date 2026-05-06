28894ac0806d2df3d0d66909c4f9a5a123660ba3
Anas Abdullfattah <anasAbdullfattah@gmail.com>
2026-05-06 14:03:25 +0300

    Exception

--- common.h
+++ common.h
@@ -23,10 +23,16 @@ typedef uint32_t vaddr_t;
 #define va_arg   __builtin_va_arg
 
 
-void *memset(void *buf, char c, size_t n);
+void *memset(void *buf, int c, size_t n);
 void *memcpy(void *dst, const void *src, size_t n);
 char *strcpy(char *dst, const char *src);
 int strcmp(const char *s1, const char *s2);
 
 
 void printf(const char *fmt, ...);
+
+#define PANIC(fmt, ...) \
+    do { \
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+        while (1) {} \
+    } while (0)
